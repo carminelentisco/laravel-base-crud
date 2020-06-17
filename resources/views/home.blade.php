e90ee42bf03a45acbff161b86ddf62f317f88fb1
carminelentisco <carminelentisco@gmail.com>
2020-06-17 19:33:44 +0200

    Indentazione corretta

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,9 +3,7 @@
 @section('main-content')
 
     <div class="container d-flex justify-content-center">
-        
         <a href="{{ route('students.index') }}" class="btn btn-success"> Show All Student</a>
-  
     </div>
 
 @endsection
