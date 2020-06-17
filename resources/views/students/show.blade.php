8fd9c56cdc8ebe2720e07a88418ca9d42dffad6a
carminelentisco <carminelentisco@gmail.com>
2020-06-17 19:33:13 +0200

    Modifiche estetiche

--- resources/views/students/show.blade.php
+++ resources/views/students/show.blade.php
@@ -6,30 +6,43 @@
         <h1 class="mb-5">Student Show</h1>
 
         <section class="d-flex justify-content-center">
-            <div class="card mr-5 mb-5" style="width: 18rem;">
+            <div class="card">
+               
+                <h5 class="card-header">{{ $student->name }}</h5>
+                
                 <div class="card-body">
-                    <h5 class="card-title">Student Name:</h5>
-                    <h6 class="card-subtitle mb-2 text-muted">{{ $student->name }}</h6>
-                    <h5>Age:</h5>
-                    <h6 class="card-subtitle mb-2 text-muted">{{ $student->age }}</h6>
-                    <h5>Date of birth:</h5>
-                    <h6 class="card-subtitle mb-2 text-muted">{{ $student->date_of_birth }}</h6>
-                    <h5>Birth place:</h5>
-                    <h6 class="card-subtitle mb-2 text-muted">{{ $student->birth_place }}</h6>
-                    <h5>Class:</h5>
-                    <h6 class="card-subtitle mb-2 text-muted">{{ $student->class }}</h6>
-                    <h5>Address of studies:</h5>
-                    <h6 class="card-subtitle mb-2 text-muted">{{ $student->address_of_studies }}</h6>
+                    <table class="table">
+                        <thead>
+                          <tr>
+                            <th scope="col">AGE</th>
+                            <th scope="col">DATE OF BIRTH</th>
+                            <th scope="col">BIRT PLACE</th>
+                            <th scope="col">CLASS</th>
+                            <th scope="col">ADDRESS OF STUDIES</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          <tr>
+                            <th scope="row">{{ $student->age }}</th>
+                            <td>{{ $student->date_of_birth }}</td>
+                            <td>{{ $student->birth_place }}</td>
+                            <td>{{ $student->class }}</td>
+                            <td>{{ $student->address_of_studies }}</td>
+                          </tr>
+                        </tbody>
+                      </table>
                 </div>
+                
+                <div class="card-footer text-muted d-flex justify-content-center">
+                    <a class="badge badge-primary mr-5" href="{{ route('home') }}">Return to Home</a>
+                    <a class="badge badge-success mr-5" href="{{ route('students.index') }}">Return to Student List</a>
+                    <a class="badge badge-danger mr-5" href="{{ route('students.create') }}">Add New Students</a>
+                </div>
+
             </div> 
         </section>
 
-        <section class="d-flex justify-content-center">
-            <a class="btn btn-primary mr-5" href="{{ route('home') }}">Return to Home</a>
-            <a class="btn btn-success mr-5" href="{{ route('students.index') }}">Return to Student List</a>
-            <a class="btn btn-danger mr-5" href="{{ route('students.create') }}">Add New Students</a>
-
-        </section>
+        
   
     </div>
 
