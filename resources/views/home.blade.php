eb3ec162a1007ede2379d659ed64810d8dbc76f3
carminelentisco <carminelentisco@gmail.com>
2020-06-17 16:47:37 +0200

    Modifiche alla pagina home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,9 @@
 
 @section('main-content')
 
-    <div class="container">
-        <h1 class="mb-5">Student Home</h1>
-
-        <section class="d-flex .justify-content-around flex-wrap">
-            @foreach($students as $student)
-                <div class="card mr-5 mb-5" style="width: 18rem;">
-                    <div class="card-body">
-                        <h5 class="card-title">Student Name:</h5>
-                        <h6 class="card-subtitle mb-2 text-muted">{{ $student->name }}</h6>
-                        <h5>Class:</h5>
-                        <h6 class="card-subtitle mb-2 text-muted">{{ $student->class }}</h6>
-                        <a href="#" class="btn btn-primary">SHOW</a>
-                    </div>
-                </div>
-            @endforeach
-        </section>
+    <div class="container d-flex justify-content-center">
+        
+        <a href="{{ route('students.index') }}" class="btn btn-success"> Show All Student</a>
   
     </div>
 
