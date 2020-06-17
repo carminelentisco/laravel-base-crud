bdb8f958ef862d85f63d07c658b4e320edf5c00e
carminelentisco <carminelentisco@gmail.com>
2020-06-17 16:48:52 +0200

    Index

---
+++ resources/views/students/index.blade.php
@@ -0,0 +1,23 @@
+@extends('layout.main')
+
+@section('main-content')
+
+    <div class="container">
+        <h1 class="mb-5">Student Home</h1>
+
+        <section class="d-flex justify-content-around flex-wrap">
+            @foreach($students as $student)
+                <div class="card  mb-5" style="width: 18rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">Student Name:</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">{{ $student->name }}</h6>
+                        <h5>Class:</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">{{ $student->class }}</h6>
+                        <a href="{{ route('students.show', $student->id) }}" class="btn btn-primary">SHOW</a>
+                    </div>
+                </div>
+            @endforeach
+        </section>
+    </div>
+
+@endsection
