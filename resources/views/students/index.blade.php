fe41c61deca71ad945f35bb20f39277bec8c5996
carminelentisco <carminelentisco@gmail.com>
2020-06-17 19:45:16 +0200

    Aggiornamenti estetici

--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -2,21 +2,25 @@
 
 @section('main-content')
 
-    <div class="container">
-        <h1 class="mb-5">Student Home</h1>
+    <div class="container mb-5">
 
-        <section class="d-flex justify-content-around flex-wrap">
-            @foreach($students as $student)
-                <div class="card  mb-5" style="width: 18rem;">
-                    <div class="card-body">
-                        <h5 class="card-title">Student Name:</h5>
-                        <h6 class="card-subtitle mb-2 text-muted">{{ $student->name }}</h6>
-                        <h5>Class:</h5>
-                        <h6 class="card-subtitle mb-2 text-muted">{{ $student->class }}</h6>
-                        <a href="{{ route('students.show', $student->id) }}" class="btn btn-primary">SHOW</a>
+        <section class="card">
+            <h2 class="card-header text-center mb-5">STUDENT LIST</h2>
+
+            <div class="d-flex justify-content-around flex-wrap">
+                @foreach($students as $student)
+                    <div class="card d-flex flex-column align-items-center  mb-5" style="width: 18rem;">
+                        <div class="card-body text-center">
+                            <h5 class="card-title">Student Name:</h5>
+                            <h6 class="card-subtitle mb-2 text-muted">{{ $student->name }}</h6>
+                            <h5>Class:</h5>
+                            <h6 class="card-subtitle mb-2 text-muted">{{ $student->class }}</h6>
+                            <a href="{{ route('students.show', $student->id) }}" class="btn btn-primary">SHOW</a>
+                        </div>
                     </div>
-                </div>
-            @endforeach
+                @endforeach
+            </div>
+
         </section>
     </div>
 
