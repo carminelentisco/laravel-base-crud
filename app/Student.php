25004762449337adda7f5598dcfe8498bca7a5c9
carminelentisco <carminelentisco@gmail.com>
2020-06-17 16:46:12 +0200

    Student model - aggiunta whitelist

--- app/Student.php
+++ app/Student.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Student extends Model
 {
-    //
+    protected $fillables = [
+        'name',
+        'age',
+        'date_of_birth',
+        'bith_place',
+        'class',
+        'address_of_studies'
+    ];
 }
