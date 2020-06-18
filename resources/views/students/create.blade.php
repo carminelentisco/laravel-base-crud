49c3553e94bc35e31624d9c52e184c7ffdf42bde
carminelentisco <carminelentisco@gmail.com>
2020-06-18 15:26:49 +0200

    Aggiunto il mantenimento dei dati nella form

--- resources/views/students/create.blade.php
+++ resources/views/students/create.blade.php
@@ -5,7 +5,7 @@
     <div class="container">
         <h1 class="mb-5">Add New Student</h1>
 
-        // Controllo delle informazioni
+        {{-- Controllo delle informazioni --}}
         @if ($errors->all())
             <div class="alert alert-danger">
                 <ul>
@@ -22,27 +22,27 @@
             @method('POST')
             
             <div class="form-group">
-                <input class="form-control" type="text" name="name" placeholder="Name student">
+                <input class="form-control" type="text" name="name" placeholder="Name student" value="{{ old('name') }}">
             </div>
             <div class="form-group">
-                <input class="form-control" type="number" name="age" placeholder="Age">
+                <input class="form-control" type="number" name="age" placeholder="Age" value="{{ old('age') }}">
             </div>
             <div class="form-group">
-                <input class="form-control" type="text" name="date_of_birth" placeholder="Date of birth">
+                <input class="form-control" type="text" name="date_of_birth" placeholder="Date of birth" value="{{ old('date_of_birth') }}">
             </div>
             <div class="form-group">
-                <input class="form-control" type="text" name="birth_place" placeholder="Birth place">
+                <input class="form-control" type="text" name="birth_place" placeholder="Birth place" value="{{ old('birth_place') }}">
             </div>
             <div class="form-group">
-                <input class="form-control" type="text" name="class" placeholder="Class">
+                <input class="form-control" type="text" name="class" placeholder="Class" value="{{ old('class') }}">
             </div>
             <div class="form-group">
-                <input class="form-control" type="text" name="address_of_studies" placeholder="Addres of studies">
+                <input class="form-control" type="text" name="address_of_studies" placeholder="Addres of studies" value="{{ old('address_of_studies') }}">
             </div>
             <input class="btn btn-success" type="submit" value="Create">
         </form>
         
-        // Action
+         {{-- Action --}}
         <section class="d-flex justify-content-center">
             <a class="btn btn-primary mr-5" href="{{ route('home') }}">Return to Home</a>
             <a class="btn btn-success mr-5" href="{{ route('students.index') }}">Return to Student List</a>
